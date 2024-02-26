0708b1d7c99f014c4e920a300223c5f14cb64406
Gayan-98 <dongayanpathirage@gmail.com>
2024-02-26 17:54:20 +0530

    intial commit

---
+++ bracket/BracketBalancer.java
@@ -0,0 +1,52 @@
+package bracket;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BracketBalancer {
+
+    public static boolean checker( String input){
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch: input.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[' ){
+                stack.push(ch);
+            }else if (ch == ')' && !stack.isEmpty() && stack.peek()== '(') {
+                stack.pop();
+            }else if (ch == ']' && !stack.isEmpty() && stack.peek()== '[') {
+                stack.pop();
+            }else if (ch == '}' && !stack.isEmpty() && stack.peek()== '{') {
+                stack.pop();
+            }else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter input 1");
+        String input1 = scanner.nextLine();
+
+        System.out.println("Enter input 2");
+        String input2 = scanner.nextLine();
+
+        System.out.println("Enter input 3");
+        String input3 = scanner.nextLine();
+
+
+        //check the balancing
+        System.out.println("input 1 balanced ? " + checker(input1));
+        System.out.println("input 2 balanced ? " + checker(input2));
+        System.out.println("input 3 balanced ? " + checker(input3));
+
+
+
+    }
+
+}
